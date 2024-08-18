83ee1d49eff836cb74d726070034cf9bdc8ffee5
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-08-17 23:05:46 -0700

    all changes to about page

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", function() {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = "-80px"; // Hide the navbar when scrolling down
-    } else {
-        navbar.style.top = "0"; // Show the navbar when scrolling up
-    }
-    lastScrollTop = scrollTop;
-});
+window.addEventListener("scroll",(e)=>{
+    document.querySelector(".background").style.backgroundSize = `${window.scrollY * 1.8 + 1600}px`
+    document.querySelector("#H1").style.opacity = `${(-window.scrollY + 300) * .004}`
+})
 
