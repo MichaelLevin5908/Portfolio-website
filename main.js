4f5ac2e71f558ffa6a5d3777dd6628957f49b5e5
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-08-18 02:22:45 -0700

    added new files to about

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-window.addEventListener("scroll",(e)=>{
-    document.querySelector(".background").style.backgroundSize = `${window.scrollY * 1.8 + 1600}px`
-    document.querySelector("#H1").style.opacity = `${(-window.scrollY + 300) * .004}`
-})
+window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    const offset = 120; // Offset to account for the navbar height
 
+    document.querySelector(".Home h1").style.marginTop = (scrollY - offset) * 1.8 + "px";
+    document.querySelector(".Home .mountains").style.marginTop = (scrollY - offset) * 1.8 + "px";
+    document.querySelector(".Home .hill-1").style.marginTop = (scrollY - offset) * 1.6 + "px";
+    document.querySelector(".Home .hill-2").style.marginTop = (scrollY - offset) * 1.4 + "px";
+    document.querySelector(".Home .leaf-1").style.marginRight = (scrollY - offset) * 1.2 + "px";
+    document.querySelector(".Home .leaf-2").style.marginLeft = (scrollY - offset) * 1.2 + "px";
+    document.querySelector(".Home .leaf-1").style.marginTop = (scrollY - offset) * 0.4 + "px";
+    document.querySelector(".Home .leaf-2").style.marginTop = (scrollY - offset) * 0.4 + "px";
+});
