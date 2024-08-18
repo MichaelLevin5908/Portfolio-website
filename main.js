51570b525f48d19922bbfe056e51620d481f4ff0
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-08-18 03:09:20 -0700

    mostly fixed

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-    const offset = 120; // Offset to account for the navbar height
-
-    document.querySelector(".Home h1").style.marginTop = (scrollY - offset) * 1.8 + "px";
-    document.querySelector(".Home .mountains").style.marginTop = (scrollY - offset) * 1.8 + "px";
-    document.querySelector(".Home .hill-1").style.marginTop = (scrollY - offset) * 1.6 + "px";
-    document.querySelector(".Home .hill-2").style.marginTop = (scrollY - offset) * 1.4 + "px";
-    document.querySelector(".Home .leaf-1").style.marginRight = (scrollY - offset) * 1.2 + "px";
-    document.querySelector(".Home .leaf-2").style.marginLeft = (scrollY - offset) * 1.2 + "px";
-    document.querySelector(".Home .leaf-1").style.marginTop = (scrollY - offset) * 0.4 + "px";
-    document.querySelector(".Home .leaf-2").style.marginTop = (scrollY - offset) * 0.4 + "px";
+window.addEventListener("scroll",()=>{
+    document.querySelector(".Home h1").style.marginTop = window.scrollY * 1.8 + "px";
+    document.querySelector(".Home .mountains").style.marginTop = window.scrollY * 1.8 + "px";
+    document.querySelector(".Home .hill-1").style.marginTop = window.scrollY * 1.6 + "px";
+    document.querySelector(".Home .hill-2").style.marginTop = window.scrollY * 1.4 + "px";
+    document.querySelector(".Home .leaf-1").style.marginRight = window.scrollY * 1.2 + "px";
+    document.querySelector(".Home .leaf-2").style.marginLeft = window.scrollY * 1.2 + "px";
+    document.querySelector(".Home .leaf-1").style.marginTop = window.scrollY * .4 + "px";
+    document.querySelector(".Home .leaf-2").style.marginTop = window.scrollY * .4 + "px";
 });
